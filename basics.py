b79e70eec5ed8a09647beff3cd7ca2653bcbb3ab
krishna-priya-03 <kpriye74@gmail.com>
2026-07-14 14:39:24 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,8 @@
+a=15
+b=15
+print("Addition =",a + b)
+print("Subtraction =",a - b)
+print("Multiplication =",a * b)
+print("Division =",a / b)
+name="sruthy"
+print("Welcomem", name)
---
+++ matplotlib_demo.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[10,20,15,30,25]
+plt.plot(x,y,marker='o')
+plt.title("Line Graph")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+plt.grid(True)
+plt.show()
